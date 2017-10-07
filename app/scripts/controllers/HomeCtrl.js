6b98f50ceb2b849bc6e13fa2734379fbe7b1947a
Anthony Zardis <anthonyzardis@gmail.com>
2017-10-06 17:51:57 -0700

    Tasks displaying

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,22 @@
 (function() {
-	function HomeCtrl(Task) {
-		this.tasks = Task.all;
+	function HomeCtrl($firebaseArray) {
+		var ref = firebase.database().ref().child('tasks');
+		this.tasks = $firebaseArray(ref);
+		
+		this.addTask = function(newTask, taskId) {
+			var now = new Date();
+			this.tasks.$add({ 
+				text: newTask, 
+				completed: false, 
+				expired: false, 
+				created: now.getTime() 
+			});
+			this.addTask = ' '
+		}
 	};
+
 
 	angular
 		.module('blocitoff')
-		.controller('HomeCtrl', ['Task', HomeCtrl]);
+		.controller('HomeCtrl', ['$firebaseArray', HomeCtrl]);
 })();
--- app/scripts/services/Tasks.js
+++ app/scripts/services/Tasks.js
@@ -1,18 +1,18 @@
-(function() {
-	function Task($firebaseArray) {
-		var Task = {};
-		var ref = firebase.database().ref().child("tasks");
-		var tasks = $firebaseArray(ref);
+// (function() {
+// 	function Task($firebaseArray) {
+// 		var Task = {};
+// 		var ref = firebase.database().ref().child("tasks");
+// 		var tasks = $firebaseArray(ref);
 
-		return {
-			all: tasks,
-			send: function(newTask){
-				tasks.$add(newTask)
-			}
-		}
-	};
+// 		// return {
+// 		// 	all: tasks,
+// 		// 	send: function(newTask){
+// 		// 		tasks.$add(newTask)
+// 		// 	}
+// 		// }
+// 	};
 
-	angular
-	.module('blocitoff')
-	.factory('Task', ['$firebaseArray', Task]);
-})();
+// 	angular
+// 	.module('blocitoff')
+// 	.factory('Task', ['$firebaseArray', Task]);
+// })();
